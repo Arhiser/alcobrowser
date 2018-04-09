f1fe1d61eae64c57b6efb5a53cbcb4363f0ced87
Sergey Arkhipov <arhiser@yandex.ru>
2018-04-09 21:48:48 +0300

    Basic retrofit infrastructure

---
+++ app/src/main/java/com/arhiser/alcobrowser/network/Request.java
@@ -0,0 +1,24 @@
+package com.arhiser.alcobrowser.network;
+
+import com.arhiser.alcobrowser.model.Store;
+import com.arhiser.alcobrowser.model.StoreRequestResult;
+import com.arhiser.alcobrowser.network.service.RetrofitService;
+
+import java.util.HashMap;
+
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class Request {
+
+    public static Single<StoreRequestResult> getStores(int page, int pageSize) {
+        HashMap<String, String> params = new HashMap<>();
+        params.put("page", Integer.toString(page));
+        params.put("per_page", Integer.toString(pageSize));
+
+        return RetrofitService.getApi().getStores(params)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+    }
+}
